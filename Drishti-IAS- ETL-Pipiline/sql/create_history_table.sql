3e2935e893bf75f2f06eb98eabeb7af3943fcdfa
sarilpandey <saril200434@gmail.com>
2026-03-21 16:42:14 +0530

    Delete Drishti-IAS- ETL-Pipiline directory

--- Drishti-IAS- ETL-Pipiline/sql/create_history_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTERNAL TABLE video_history (
-    video_id STRING,
-    views INT,
-    recorded_date STRING
-)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
-LOCATION 's3://your-bucket-name/youtube/history/';
